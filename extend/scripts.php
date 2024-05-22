db83b3a381335805b49aaae8eb33afc22dd048f4
angelk <danielperman12@gmail.com>
2024-05-21 23:13:45 -0600

    inicio

---
+++ extend/scripts.php
@@ -0,0 +1,7 @@
+</main>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="../js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.3.2/sweetalert2.js"></script>
+<script>
+    $('.button-collpase').sideNav();
+</script>
